5b4e809631d0cdcf490e9d5147886f93af7103af
Artsiom-Zhuk <artsiom.zhuk498@gmail.com>
2019-02-12 01:29:54 +0300

    fix: corrected errors

--- src/removeHealthBot.js
+++ src/removeHealthBot.js
@@ -3,7 +3,7 @@ import addHealthBot from "./addHealthBot";
 function removeHealthBot(){
     let idHealthBot = document.getElementById("idHealthBot");
     idHealthBot.setAttribute("value",+idHealthBot.value - 10);
-    if(+idHealthBot.value == 90){
+    if(+idHealthBot.value == 0){
         addHealthBot();
         let idResponseAnswerUser = document.getElementById("idResponseAnswerUser");
         idResponseAnswerUser.style.display = "none";
